b5fa332a1a5d1012fd55be50a5c6b0e9d8032676
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-02-05 22:15:41 -0600

    edit

--- back_end.py
+++ back_end.py
@@ -2,10 +2,47 @@
 import mysql.connector
 from mysql.connector import errorcode
 
-mydb = mysql.connector.connect(
-	host="localhost",
-	user="root",
-	password="yafvr8udisspac3exec"
-	)
+# mydb = mysql.connector.connect(
+# 	host="localhost",
+# 	user="root",
+# 	password="password",
+# 	database="testdb"
+# 	)
+#
+# cur = mydb.cursor()
+#
+#
+# cur.execute("""CREATE TABLE students(
+# 			first_name VARCHAR(30),
+# 			last_name VARCHAR(30),
+# 			age INT(10)
+# 			)
+# 			""")
+# cur.execute("""SHOW TABLES""")
+#
+# for tb in cur:
+# 	print(tb)
 
-print(mydb)
+class newDatabase:
+	def __init__(self, database):
+		self.database = mysql.connector.connect(
+			host="localhost",
+			user="root",
+			password="password",
+			database="testdb2"
+		)
+		self.cursor=self.database.cursor()
+		self.ConnectionTest()
+
+
+	def ConnectionTest(self):
+		try:
+			print(self.database)
+		except Exception as e:
+			print(e)
+
+
+
+if __name__ == '__main__':
+	db='test2DB'
+	dbu=newDatabase(db)
