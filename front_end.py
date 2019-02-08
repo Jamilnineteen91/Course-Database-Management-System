715e010d433d5f874884cee0e8dc3835390a3321
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-02-07 22:20:58 -0600

    created add function

--- front_end.py
+++ front_end.py
@@ -7,8 +7,12 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from back_end import *
 
 class Ui_MainWindow(object):
+    db=newDatabase()
+    cursor=db.cursor()
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1172, 553)
@@ -44,6 +48,7 @@ class Ui_MainWindow(object):
         self.studentRadioButton.setMaximumSize(QtCore.QSize(100, 100))
         self.studentRadioButton.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.studentRadioButton.setObjectName("studentRadioButton")
+
         self.horizontalLayout_3.addWidget(self.studentRadioButton)
         self.teacherRadioButton = QtWidgets.QRadioButton(self.centralwidget)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
@@ -55,6 +60,7 @@ class Ui_MainWindow(object):
         self.teacherRadioButton.setSizeIncrement(QtCore.QSize(0, 0))
         self.teacherRadioButton.setLayoutDirection(QtCore.Qt.LeftToRight)
         self.teacherRadioButton.setObjectName("teacherRadioButton")
+
         self.horizontalLayout_3.addWidget(self.teacherRadioButton)
         self.verticalLayout_2.addLayout(self.horizontalLayout_3)
         spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
@@ -73,54 +79,63 @@ class Ui_MainWindow(object):
         self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.firstNameLabel)
         self.firstNameLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.firstNameLineEdit.setObjectName("firstNameLineEdit")
+
         self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.firstNameLineEdit)
         self.lastNameLabel = QtWidgets.QLabel(self.centralwidget)
         self.lastNameLabel.setObjectName("lastNameLabel")
         self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.lastNameLabel)
         self.lastNameLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.lastNameLineEdit.setObjectName("lastNameLineEdit")
+
         self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.lastNameLineEdit)
         self.genderLabel = QtWidgets.QLabel(self.centralwidget)
         self.genderLabel.setObjectName("genderLabel")
         self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.genderLabel)
         self.genderLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.genderLineEdit.setObjectName("genderLineEdit")
+
         self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.genderLineEdit)
         self.addressLabel = QtWidgets.QLabel(self.centralwidget)
         self.addressLabel.setObjectName("addressLabel")
         self.formLayout.setWidget(4, QtWidgets.QFormLayout.LabelRole, self.addressLabel)
         self.addressLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.addressLineEdit.setObjectName("addressLineEdit")
+
         self.formLayout.setWidget(4, QtWidgets.QFormLayout.FieldRole, self.addressLineEdit)
         self.cityLabel = QtWidgets.QLabel(self.centralwidget)
         self.cityLabel.setObjectName("cityLabel")
         self.formLayout.setWidget(5, QtWidgets.QFormLayout.LabelRole, self.cityLabel)
         self.cityLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.cityLineEdit.setObjectName("cityLineEdit")
+
         self.formLayout.setWidget(5, QtWidgets.QFormLayout.FieldRole, self.cityLineEdit)
         self.regionLabel = QtWidgets.QLabel(self.centralwidget)
         self.regionLabel.setObjectName("regionLabel")
         self.formLayout.setWidget(6, QtWidgets.QFormLayout.LabelRole, self.regionLabel)
         self.regionLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.regionLineEdit.setObjectName("regionLineEdit")
+
         self.formLayout.setWidget(6, QtWidgets.QFormLayout.FieldRole, self.regionLineEdit)
         self.countryLabel = QtWidgets.QLabel(self.centralwidget)
         self.countryLabel.setObjectName("countryLabel")
         self.formLayout.setWidget(7, QtWidgets.QFormLayout.LabelRole, self.countryLabel)
         self.countryLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.countryLineEdit.setObjectName("countryLineEdit")
+
         self.formLayout.setWidget(7, QtWidgets.QFormLayout.FieldRole, self.countryLineEdit)
         self.zipLabel = QtWidgets.QLabel(self.centralwidget)
         self.zipLabel.setObjectName("zipLabel")
         self.formLayout.setWidget(8, QtWidgets.QFormLayout.LabelRole, self.zipLabel)
         self.zipLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.zipLineEdit.setObjectName("zipLineEdit")
+
         self.formLayout.setWidget(8, QtWidgets.QFormLayout.FieldRole, self.zipLineEdit)
         self.phoneNumberLabel = QtWidgets.QLabel(self.centralwidget)
         self.phoneNumberLabel.setObjectName("phoneNumberLabel")
         self.formLayout.setWidget(9, QtWidgets.QFormLayout.LabelRole, self.phoneNumberLabel)
         self.phoneNumberLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.phoneNumberLineEdit.setObjectName("phoneNumberLineEdit")
+
         self.formLayout.setWidget(9, QtWidgets.QFormLayout.FieldRole, self.phoneNumberLineEdit)
         self.verticalLayout_2.addLayout(self.formLayout)
         self.horizontalLayout_2.addLayout(self.verticalLayout_2)
@@ -143,24 +158,28 @@ class Ui_MainWindow(object):
         self.courseFormLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.nameLabel)
         self.nameLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.nameLineEdit.setObjectName("nameLineEdit")
+
         self.courseFormLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.nameLineEdit)
         self.descriptionLabel = QtWidgets.QLabel(self.centralwidget)
         self.descriptionLabel.setObjectName("descriptionLabel")
         self.courseFormLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.descriptionLabel)
         self.descriptionLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.descriptionLineEdit.setObjectName("descriptionLineEdit")
+
         self.courseFormLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.descriptionLineEdit)
         self.courseIDLabel = QtWidgets.QLabel(self.centralwidget)
         self.courseIDLabel.setObjectName("courseIDLabel")
         self.courseFormLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.courseIDLabel)
         self.courseIDLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.courseIDLineEdit.setObjectName("courseIDLineEdit")
+
         self.courseFormLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.courseIDLineEdit)
         self.teacherIDLabel = QtWidgets.QLabel(self.centralwidget)
         self.teacherIDLabel.setObjectName("teacherIDLabel")
         self.courseFormLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.teacherIDLabel)
         self.teacherIDLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.teacherIDLineEdit.setObjectName("teacherIDLineEdit")
+
         self.courseFormLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.teacherIDLineEdit)
         self.courseContainer.addLayout(self.courseFormLayout)
         self.horizontalLayout_2.addLayout(self.courseContainer)
@@ -216,6 +235,37 @@ class Ui_MainWindow(object):
         self.updateButton.setText(_translate("MainWindow", "Update"))
         self.searchButton.setText(_translate("MainWindow", "Search"))
         self.deleteButton.setText(_translate("MainWindow", "Delete"))
+
+    def add(self):
+        if self.studentRadioButton.clicked():
+            cursor.execute('USE courseDB')
+            cursor.execute("""INSERT INTO student(student_id, first_name, last_name, gender, address, city, region, country, zip phone_number)
+                            VALUES (%d, %s, %s, %s, %s, %s, %s, %s, %s, %d, %d)
+                            """ % (''.join(self.firstNameLineEdit.text()),
+                                   ''.join(self.lastNameLineEdit.text()),
+                                   ''.join(self.genderNameLineEdit.text()),
+                                   ''.join(self.addressLineEdit.text()),
+                                   ''.join(self.cityLineEdit.text()),
+                                   ''.join(self.regionLineEdit.text()),
+                                   ''.join(self.countryLineEdit.text()),
+                                   ''.join(self.zipLineEdit.text()),
+                                   ''.join(self.phoneNumberLineEdit.text())
+                                   ))
+
+        elif self.teacherRadioButton.clicked():
+            cursor.execute('USE courseDB')
+            cursor.execute("""INSERT INTO teacher(teacher_id, first_name, last_name, gender, address, city, region, country, zip phone_number)
+                            VALUES (%d, %s, %s, %s, %s, %s, %s, %s, %s, %d, %d)
+                            """ % (''.join(self.firstNameLineEdit.text()),
+                                   ''.join(self.lastNameLineEdit.text()),
+                                   ''.join(self.genderNameLineEdit.text()),
+                                   ''.join(self.addressLineEdit.text()),
+                                   ''.join(self.cityLineEdit.text()),
+                                   ''.join(self.regionLineEdit.text()),
+                                   ''.join(self.countryLineEdit.text()),
+                                   ''.join(self.zipLineEdit.text()),
+                                   ''.join(self.phoneNumberLineEdit.text())))
+
 
 
 if __name__ == "__main__":
