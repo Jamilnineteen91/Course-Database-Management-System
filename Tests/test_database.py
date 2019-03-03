5929fd0afb5c010001b99ce8d451d09c6c1e7dc3
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-03-03 16:38:35 -0600

    edited tearDown f(x)

--- Tests/test_database.py
+++ Tests/test_database.py
@@ -10,11 +10,23 @@ class TestDatabase(unittest.TestCase):
         self.db = newDatabase()
 
     def tearDown(self):
-        pass
+        self.db.cursor.execute("DROP DATABASE IF EXISTS courseDB;")
+        print('tearDown\n')
 
 
     def test_newDatabase(self):
         self.assertIsInstance(self.db,newDatabase)
+
+    def test_add_student(self):
+        self.db.add_student(7644042,'jamil','mbabaali','M','183 eastcote dr.','winnipeg','manitoba','canada','r2n4h4',2049791641)
+        self.db.cursor.execute("SELECT * FROM student WHERE student_id=7644042")
+        self.studentA=self.db.cursor.fetchall()
+
+        self.studentB=[(7644042,'jamil','mbabaali','M','183 eastcote dr.','winnipeg','manitoba','canada','r2n4h4',2049791641)]
+
+
+
+        self.assertEqual(self.db, self.studentA, type(self.studentB))
 
 
 
