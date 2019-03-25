082ad0a7c45b5b2f38a4f929fb0a8d75928f2c08
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-03-25 14:37:40 -0500

    completed coding the add button/function, ready for testing

--- gui.py
+++ gui.py
@@ -6,7 +6,8 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, QPushButton
+from database import newDatabase
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -175,6 +176,7 @@ class Ui_MainWindow(object):
         self.add_pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.add_pushButton.setObjectName("add_pushButton")
         self.horizontalLayout_2.addWidget(self.add_pushButton)
+        self.add_pushButton.clicked().connect(self.add_button())
         self.update_pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.update_pushButton.setObjectName("update_pushButton")
         self.horizontalLayout_2.addWidget(self.update_pushButton)
@@ -197,6 +199,8 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        self.db=newDatabase()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -232,6 +236,60 @@ class Ui_MainWindow(object):
         self.update_pushButton.setText(_translate("MainWindow", "Update"))
         self.search_pushButton.setText(_translate("MainWindow", "Search"))
         self.delete_pushButton.setText(_translate("MainWindow", "Delete"))
+
+    def persons_vals(self):
+        self.id=int(self.stdnt_tchr_ID_lineEdit.text())
+        self.first_name=str(self.first_name_lineEdit.text())
+        self.last_name=str(self.last_name_lineEdit.text())
+        self.gender_comboBox=self.comboBox.currentText()
+        self.address=str(self.address_lineEdit.text())
+        self.city=str(self.city_lineEdit.text())
+        self.region=str(self.region_lineEdit.text())
+        self.country=str(self.country_lineEdit.text())
+        self.zip=str(self.zip_lineEdit.text())
+        self.telephone=int(self.telephone_lineEdit.text())
+        return [self.id, self.first_name, self.last_name, self.gender_comboBox, self.address,
+        self.city, self.region, self.country, self.zip, self.telephone]
+
+    def course_vals(self):
+        self.crs_id=str(self.course_id_lineEdit.text())
+        self.crs_name=str(self.crs_name_lineEdit.text())
+        self.crs_desc=str(self.crs_desc_lineEdit.text())
+        self.crs_teacherID=int(self.crs_teacherID_lineEdit.text())
+        return [self.crs_id, self.crs_name, self.crs_desc, self.crs_teacherID]
+
+    def enroll_vals(self):
+        self.enroll_stdntID=int(self.enroll_stdntID_lineEdit.text())
+        self.enroll_crsID=str(self.enroll_crsID_lineEdit.text())
+        self.enroll_grade=str(self.enroll_grade_lineEdit.text())
+        return [self.enroll_stdntID, self.enroll_crsID, self.enroll_grade]
+
+    def empty_vals(self,array):
+        for val in array:
+            if len(val.text())>0:
+                return False
+            else:
+                return True
+
+    def add_button(self):
+        if self.stdnt_radioButton.isChecked()==True and self.empty_vals(self.persons_vals())==False:
+            self.stdnt_info=self.persons_vals()
+            self.db.add_student(self.stdnt_info[0],self.stdnt_info[1],self.stdnt_info[2],self.stdnt_info[3],
+            self.stdnt_info[4],self.stdnt_info[5],self.stdnt_info[6],self.stdnt_info[7],self.stdnt_info[8],self.stdnt_info[9])
+
+        elif self.tchr_radioButton.isChecked()==True and self.empty_vals(self.persons_vals())==False:
+            self.tchr_info=self.persons_vals()
+            self.db.add_teacher(self.tchr_info[0],self.tchr_info[1],self.tchr_info[2],self.tchr_info[3],
+            self.tchr_info[4],self.tchr_info[5],self.tchr_info[6],self.tchr_info[7],self.tchr_info[8],self.tchr_info[9])
+
+        elif self.crs_radioButton.isChecked()==True and self.empty_vals(self.course_vals())==False:
+            self.crs_info=self.course_vals()
+            self.db.add_course(crs_info[0],crs_info[1],crs_info[2],crs_info[3])
+
+        elif self.enroll_radioButton.isChecked()==True and self.empty_vals(self.enroll_vals())==False:
+            self.enroll_info=self.self.enroll_vals()
+            self.db.enroll(self.enroll_info[0],self.enroll_info[1],self.enroll_info[2])
+
 
 
 if __name__ == "__main__":
